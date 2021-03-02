4c184eb08f0e54598e8679c6092e6a88167d0430
breinerde <dennis.breiner@trumpf.com>
2021-03-02 13:35:45 +0100

    fix tests

--- Dotnet.ToolBuilder.Api.Test/Api/DotnetTool/ToolBuilderTest.cs
+++ Dotnet.ToolBuilder.Api.Test/Api/DotnetTool/ToolBuilderTest.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Dotnet.ToolBuilder.Api.Test.Base;
 using Dotnet.ToolBuilder.Api.Test.Extensions;
+using DotNetTool.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Dotnet.ToolBuilder.Api.Test.Api.DotnetTool
@@ -8,6 +10,14 @@ namespace Dotnet.ToolBuilder.Api.Test.Api.DotnetTool
     [TestClass]
     public class ToolBuilderTest : TestBase
     {
+
+        [ClassInitialize]
+        public static async Task ClassInitialize(TestContext context)
+        {
+            var dotnetTool = DotNetToolFactory.Create();
+            await dotnetTool.InstallAsync("dotnettool.builder", "9.0.0");
+        }
+
         [TestMethod]
         public async Task Should_return_Success_StatusCode()
         {
@@ -24,6 +34,13 @@ namespace Dotnet.ToolBuilder.Api.Test.Api.DotnetTool
             var result = await Client.PostAsJsonStringAsync("api/toolbuilder/create-unsafe", testTool).ConfigureAwait(false);
 
             Assert.IsTrue(result.IsSuccessStatusCode);
+        }
+
+        [ClassCleanup]
+        public static async Task Cleanup()
+        {
+            var dotnetTool = DotNetToolFactory.Create();
+            await dotnetTool.UninstallAsync("dotnettool.builder", "9.0.0");
         }
     }
 }
